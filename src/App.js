e63242aaf506a518bc9d6ea3fd4d67fc9c25b2ca
siwanijaiswal <siwani.jce20@sot.pdpu.ac.in>
2023-08-21 12:59:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
+
+  import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [unit, setUnit] = useState('C');
+  const [forecastData, setForecastData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const apiKey = '9133cee7bcde4363a7571647232108'; // Replace with your actual API key
+
+  const fetchWeather = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3`);
+      const data = await response.json();
+      setWeatherData(data.current);
+      setForecastData(data.forecast.forecastday);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      setWeatherData(null);
+      setForecastData([]);
+      setIsLoading(false);
+    }
+  };
+
+  const handleUnitChange = () => {
+    setUnit(unit === 'C' ? 'F' : 'C');
+  };
+
+  const getLocationWeather = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
+          try {
+            setIsLoading(true);
+            const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latitude},${longitude}&days=3`);
+            const data = await response.json();
+            setWeatherData(data.current);
+            setForecastData(data.forecast.forecastday);
+            setIsLoading(false);
+          } catch (error) {
+            console.error('Error fetching weather data:', error);
+            setWeatherData(null);
+            setForecastData([]);
+            setIsLoading(false);
+          }
+        },
+        (error) => {
+          console.error('Error getting geolocation:', error);
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    getLocationWeather();
+  }, []); // Runs once when the component mounts
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={fetchWeather}>Search</button>
+      <button onClick={getLocationWeather}>Use My Location</button>
+      {isLoading && <p>Loading...</p>}
+      {weatherData && (
+        <div className="weather-card">
+          <h2>{city}</h2>
+          <p>{unit === 'C' ? `${weatherData.temp_c}°C` : `${weatherData.temp_f}°F`}</p>
+          <p>{weatherData.condition.text}</p>
+          <img src={weatherData.condition.icon} alt="Weather Icon" />
+          <button onClick={handleUnitChange}>Toggle {unit === 'C' ? 'Fahrenheit' : 'Celsius'}</button>
+        </div>
+      )}
+      <div className="forecast">
+        <h2>3-Day Forecast</h2>
+        <ul>
+          {forecastData.map((day) => (
+            <li key={day.date}>
+              <p>{day.date}</p>
+              <p>{day.day.avgtemp_c}°C | {day.day.avgtemp_f}°F</p>
+              <p>{day.day.condition.text}</p>
+              <img src={day.day.condition.icon} alt="Weather Icon" />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +10,63 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// import React, { useState, useEffect, useCallback } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [weather, setWeather] = useState({});
+//   const [locations, setLocations] = useState("ahemdabad");
+
+//   const fetchWeather = useCallback(() => {
+//     fetch(
+//       `https://api.weatherapi.com/v1/current.json?key={YOUR_WEATHERAPI_KEY}&q=${locations}&aqi=no`
+//     )
+//       .then((res) => {
+//         if (res.ok) {
+//           return res.json();
+//         } else {
+//           if (res.status === 400) {
+//             alert("wrong location");
+//           } else {
+//             alert("there is error");
+//           }
+//           throw new Error("Weather API error");
+//         }
+//       })
+//       .then((data) => {
+//         setWeather(data);
+//       })
+//       .catch((error) => console.log(error));
+//   }, [locations]);
+
+//   useEffect(() => {
+//     fetchWeather();
+//   }, [fetchWeather]);
+
+//   return (
+//     <div className="app">
+//       <div className="wrapper">
+//         <div className="search">
+//           <input
+//             type="text"
+//             value={locations}
+//             onChange={(e) => setLocations(e.target.value)}
+//             placeholder="Enter location"
+//             className="location_input"
+//           />
+//           <button className="location_searcher" onClick={fetchWeather}>
+//             Search Location
+//           </button>
+//         </div>
+//         <div className="app__data">
+//           <p className="temp">
+//             Current Temperature: {weather?.current?.temp_c}°C
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
